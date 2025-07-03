b642c04c4e346f238bfc227e1f7b2daa3adac8ea
Beau Raines <beau.raines@gmail.com>
2025-07-02 20:37:24 -0700

    additioanl test string

--- test/cli.test.js
+++ test/cli.test.js
@@ -22,5 +22,8 @@ describe('CLI Pig Latin Translator', () => {
   it('should handle input from STDIN', () => {
     const output = execSync('echo \"Hello STDIN\" | node cli.js --no-wrapper').toString();
     expect(output).toContain('Ellohay INSTDAY'); // Updated to match actual output
+
+    const output2 = execSync('echo \"The quick brown fox jumps over the lazy dog.\" | node cli.js --no-wrapper').toString();
+    expect(output2).toContain('Ethay uickqay ownbray oxfay umpsjay overyay ethay azylay ogday'); // Updated to match actual output
   });
 });
